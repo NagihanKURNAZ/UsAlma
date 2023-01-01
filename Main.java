888121936783df675ba57919126cc1f1484096f8
NagihanKURNAZ <112775522+NagihanKURNAZ@users.noreply.github.com>
2023-01-01 11:57:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int n, k;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Üssü alınacak sayı: ");
+		n = scanner.nextInt();
+		System.out.print("Üs olacak sayı: ");
+		k = scanner.nextInt();
+		
+		int total=1;
+		
+		for (int i = 1; i<=k ; i++) {
+			total *=n;
+		}
+	System.out.println("Sonuç:" +total);
+
+	}
+
+}
